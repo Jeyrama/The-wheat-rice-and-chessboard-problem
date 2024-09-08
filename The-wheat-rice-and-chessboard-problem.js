dd22be28613a8ff44bbf8a27294062ed30cf5e81
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-09-08 14:23:33 -0700

    Update The-wheat-rice-and-chessboard-problem.js

--- The-wheat-rice-and-chessboard-problem.js
+++ The-wheat-rice-and-chessboard-problem.js
@@ -1,6 +1,5 @@
 /*
-I assume most of you are familiar with the ancient legend of the rice 
-(but I see wikipedia suggests wheat, for some reason) problem, 
+I assume most of you are familiar with the ancient legend of the rice problem, 
 but a quick recap for you: a young man asks as a compensation only 1 grain of rice for the first square, 
 2 grains for the second, 4 for the third, 8 for the fourth and so on, always doubling the previous.
 
@@ -16,10 +15,7 @@ As usual, a few examples might be way better than thousands of words from me:
   4 grains need 3 cells
   and etc.
 
-Input is always going to be valid/reasonable: ie: a non negative number; 
-extra cookie for not using a loop to compute square-by-square (at least not directly) 
-and instead trying a smarter approach [hint: some peculiar operator]; 
-a trick converting the number might also work: impress me!
+Input is always going to be valid/reasonable, in other words, a non negative number; 
 */
 
 
